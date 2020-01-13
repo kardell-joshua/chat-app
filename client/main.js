d64a7742963a2fc487b39962032720fe0aa645e3
Josh <kardelljoshua@gmail.com>
2020-01-12 19:57:38 -0600

    make it better

---
+++ client/main.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+$('document').ready(function() {
+    $('form').on('submit', function(e) {
+        var text = $('#message').val();
+        socket.emit('message', text);
+        $('#message').val('');
+        e.preventDefault();
+        return false;
+    });
+
+    socket.on('message', function(msg) {
+        if (!msg) {
+            return;
+        }
+        $('#history').append([
+            $('<p>', { class: 'message' }).text(msg),
+            $('<div>', { class: 'clear' })
+        ]);
+        $('#history').scrollTop($('#history')[0].scrollHeight);
+    });
+});
