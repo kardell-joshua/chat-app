75c98e565eeb738a4e880d95f080280bebe467c0
Josh <kardelljoshua@gmail.com>
2020-02-08 15:43:17 -0600

    adding index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
+// creates the express js app
 var app = express();
 
 var http = require('http');
+
 var server = http.Server(app);
 
+// serves the static files in client html, css, js etc
 app.use(express.static('client'));
 
 server.listen(PORT, function() {
