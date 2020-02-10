a5df53f977fbb5de0cb390c97494c739e044407a
Josh <kardelljoshua@gmail.com>
2020-02-09 18:16:58 -0600

    Styles and some functionality added

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 var socket = io();
 
 $('document').ready(function() {
+    var user = prompt('Please enter your name: ');
+    var chatBoolean = true;
+
     $('form').on('submit', function(e) {
         var text = $('#message').val();
         socket.emit('message', text);
@@ -17,10 +20,30 @@ $('document').ready(function() {
         }
 
         $('#history').append([
-            $('<p>', { class: 'time' }).text('Posted at: ' + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true})),
+            $('<p>', { class: 'time' }).text('Posted  by: ' +  user + ' at: ' + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true})),
             $('<p>', { class: 'message past-message' }).text(msg),
             $('<div>', { class: 'clear' })
         ]);
         $('#history').scrollTop($('#history')[0].scrollHeight);
+    });
+
+    $('#header-text-task').click(function() {
+        if (chatBoolean) {
+            $('#chat-wrapper').hide("slow");
+            $('#task-wrapper').show("slow");
+            $('#header-text-chat').css('color', 'black');
+            $('#header-text-task').css('color', '#d3d3d3');
+            chatBoolean = false;
+        }
+    });
+
+    $('#header-text-chat').click(function() {
+        if (!chatBoolean) {
+            $('#chat-wrapper').show("slow");
+            $('#task-wrapper').hide("slow");
+            $('#header-text-chat').css('color', '#d3d3d3');
+            $('#header-text-task').css('color', 'black');
+            chatBoolean = true;
+        }
     });
 });
